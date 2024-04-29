cd5e34f9f38a6d9af58038869ced1ea9268e1663
lyae <lyaesley@gmail.com>
2024-04-29 23:57:16 +0900

    Initial commit

---
+++ src/main/java/hello/thymeleaf/Springmvc2thymeleafApplication.java
@@ -0,0 +1,13 @@
+package hello.thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springmvc2thymeleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springmvc2thymeleafApplication.class, args);
+    }
+
+}
